10b6b586e696d0cfa8ff90406fa1c7521c6c1502
Josefisu29 <151913443+Josefisu29@users.noreply.github.com>
2024-10-03 13:02:59 -0700

    up

--- Game.js
+++ Game.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const choices = document.querySelectorAll(".choice");
   const resultText = document.getElementById("read_result");
+
   choices.forEach((choice) => {
     choice.addEventListener("click", () => {
       const playerChoice = choice.getAttribute("play");
@@ -9,22 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
       resultText.textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
     });
   });
+
   function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
   function determineWinner(player, computer) {
-    if (player == computer) {
+    if (player === computer) {
       return "It's a draw! Try again 🥹";
     } else if (
-      (player == "rock" && computer == "scissors")||
-      (player == "paper" && computer == "rock")||
-      (player == "scissors" && computer == "paper")||
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
     ) {
       return "You win Yayyy!!! 🥳";
     } else {
-      return " Sorry,You lose!🥺";
+      return "Sorry, You lose! 🥺";
     }
   }
 });
